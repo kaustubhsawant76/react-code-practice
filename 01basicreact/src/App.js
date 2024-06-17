c8e6970b17e9513923fe7c3eba9cff723569d3a8
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-06-17 14:33:15 +0000

    injecting html in js and fragments

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Demo from './demo';
 
 function App() {
   return (
+    <>
     <h1>MY name is kaustubh</h1>
+    <Demo/>
+    </>
   );
 }
 
