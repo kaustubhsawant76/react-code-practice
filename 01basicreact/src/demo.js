6928adafaccc8330a4d7eb6b64abc026e8950af5
Kaustubh Sawant <145922083+kaustubhsawant76@users.noreply.github.com>
2024-12-13 07:11:41 +0000

    deleted

--- 01basicreact/src/demo.js
+++
@@ -1,8 +0,0 @@
-function  Demo(){
-    return(
-
-        <h1>MY SURNAME IS SAWANT</h1>
-    );
-}
-
-export default Demo
--- 01vitebasicreact/src/demo.jsx
+++
@@ -1,9 +0,0 @@
-function Demo(){
-    return(
-
-
-        <h1>I AM A DEVELOPER</h1>
-    );
-}
-
-export default Demo;
