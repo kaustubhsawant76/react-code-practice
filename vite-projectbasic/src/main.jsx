db3be9438acebfcafcd996a333e74e299295474f
Kaustubh Sawant <145922083+kaustubhsawant76@users.noreply.github.com>
2024-12-16 13:07:03 +0000

    React element

--- vite-projectbasic/src/main.jsx
+++ vite-projectbasic/src/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return(
+    <div>
+      <h1>Custom App !!!</h1>
+    </div>
+  )
+}
+
+
+//JSX is parsed into following format
+//the below code is not working the problem is with the syntax follow anothjerElement syntac
+const ReactElement = {
+  type:'a',
+  props: {
+      href:'https://google.com',
+      target:'_blank'
+  },
+  children:'Click me to visit google'
+  }
+  
+  const anotherElement = (
+    <a href="https://google.com" target="_blank">Visit Google</a>
+  )
+
+createRoot(document.getElementById('root')).
+render(
+  
+    anotherElement
+
 )
